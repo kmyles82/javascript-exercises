6f5c1ece2c8dc3f5987053f9a0fcf810cf9dda11
kmyles82 <kmyles82@gmail.com>
2018-08-30 14:28:48 -0500

    completed palidrome javascript exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,12 +1,18 @@
 const palindromes = function(word) {
     let arr = []
-    let new_arr = []
-    arr = word.split("")
+    let str = word.toLowerCase().replace(/[^A-Za-z]/g, "");
+    // let new_arr = []
+    arr = str.split("").reverse().join("")
     // console.log(arr)
-    new_arr = arr.reverse()
+    // new_arr = arr.reverse()
     // console.log(arr)
-    new_arr = new_arr.join("")
-    return new_arr;
+    // new_arr = new_arr.join("")
+    if (arr === str) {
+        return true;    
+    } else {
+        return false;
+    }
+    
 }
 
 module.exports = palindromes
